ba363468c88746ce7720eb3447f9402fffb76598
navid1111 <navidkamal568@gmail.com>
2025-11-02 12:50:55 +0600

    Update import path for createElement and render from didact.js to navid.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createElement, render } from "./core/didact.js";
+import { createElement, render } from "./core/navid.js";
 import App from "./app/App.js";
 
 const element = createElement(App, { name: "Foo" });
